e70a134ba0cabc012bf799d5acda5bf5d8830a39
BaldonValentine <valentine.baldon@gmail.com>
2022-01-26 23:01:59 +0100

    feature 2 5 et 6

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -5,9 +5,11 @@
 let currentProducts = [];
 let currentPagination = {};
 
-// inititiqte selectors
+// inititiate selectors
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
+const selectBrand = document.querySelector('#brand-select')
+const selectSort = document.querySelector('#sort-select')
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
 
@@ -50,6 +52,8 @@ const fetchProducts = async (page = 1, size = 12) => {
  * Render list of products
  * @param  {Array} products
  */
+
+
 const renderProducts = products => {
     const fragment = document.createDocumentFragment();
     const div = document.createElement('div');
@@ -86,6 +90,27 @@ const renderPagination = pagination => {
     selectPage.selectedIndex = currentPage - 1;
 };
 
+// Render Brand selector
+
+let renderBrand = products => {
+
+    let brandName = products.map(product => product.brand)
+    let brandUnique = [];
+    brandName.forEach(item => {
+        if (brandUnique.includes(item)) { }
+        else { brandUnique.push(item) }
+    })
+
+    let options = ['<option value="...">...</option>']
+    options.push(Array.from(
+        { 'length': brandUnique.length },
+        (value, index) => `<option value="${brandUnique[index]}">${brandUnique[index]}</option>`
+    ).join(''));
+    options.push('<option value="all">all</option>')
+
+    selectBrand.innerHTML = options;
+};
+
 /**
  * Render page selector
  * @param  {Object} pagination
@@ -100,8 +125,52 @@ const render = (products, pagination) => {
     renderProducts(products);
     renderPagination(pagination);
     renderIndicators(pagination);
+    renderBrand(products);
 };
 
+function sortByPrice(items) {
+    return items.result.sort(
+        function (a, b) {
+            if (parseFloat(a.price) - parseFloat(b.price) > 0) {
+                return 1;
+            }
+            else if (parseFloat(a.price) - parseFloat(b.price) == 0) {
+                return 0;
+            }
+            else (parseFloat(a.price) - parseFloat(b.price) < 0)
+            {
+                return -1;
+            }
+        });
+}
+function sortByPriceDesc(items) {
+    return items.result.sort(
+        function (a, b) {
+            if (parseFloat(a.price) - parseFloat(b.price) > 0) {
+                return -1;
+            }
+            else if (parseFloat(a.price) - parseFloat(b.price) == 0) {
+                return 0;
+            }
+            else (parseFloat(a.price) - parseFloat(b.price) < 0)
+            {
+                return 1;
+            }
+        });
+}
+function sortByDate(items) {
+    return items.result.sort(
+    function (a, b) 
+    {
+        return new Date(b.released) - new Date(a.released);
+    });
+};
+function sortByDateDesc(items) {
+    return items.result.sort(
+    function (a, b) {
+        return new Date(a.released) - new Date(b.released);
+    });
+};
 /**
  * Declaration of all Listeners
  */
@@ -110,10 +179,16 @@ const render = (products, pagination) => {
  * Select the number of products to display
  * @type {[type]}
  */
+let pagebase = 1;
+let sortpa = false;
+let sortpd = false;
+let sortda = false;
+let sortdd = false;
 selectShow.addEventListener('change', event => {
-    fetchProducts(currentPagination.currentPage, parseInt(event.target.value))
+    fetchProducts(pagebase, parseInt(event.target.value))
         .then(setCurrentProducts)
-        .then(() => render(currentProducts, currentPagination));
+        .then(() => render(currentProducts, currentPagination))
+        .then(selectSort.value = "none");
 });
 
 document.addEventListener('DOMContentLoaded', () =>
@@ -122,8 +197,46 @@ document.addEventListener('DOMContentLoaded', () =>
         .then(() => render(currentProducts, currentPagination))
 );
 
-selectPage.addEventListener('change', event => {
-    fetchProducts(parseInt(event.target.value), parseInt(selectShow.value))
-        .then(setCurrentProducts)
-        .then(() => render(currentProducts, currentPagination));
+selectPage.addEventListener('change', async (event) => {
+    const products = await fetchProducts(parseInt(event.target.value), parseInt(selectShow.value));
+    setCurrentProducts(products);
+    render(currentProducts, currentPagination);
+    selectSort.value = "none";
+});
+
+selectBrand.addEventListener('change', async (event) => {
+    let products = await fetchProducts(currentPagination.currentPage, selectShow.value);
+
+    if (event.target.value == "all") {
+        setCurrentProducts(products);
+        render(currentProducts, currentPagination);
+        selectSort.value = "none";
+    }
+    else {
+        products.result = products.result.filter(item => item.brand == event.target.value);
+        setCurrentProducts(products);
+        render(currentProducts, currentPagination);
+        selectSort.value = "none";
+    }
+});
+
+selectSort.addEventListener('change', async (event) => {
+    let products = await fetchProducts(currentPagination.currentPage, selectShow.value);
+    switch (event.target.value) {
+        case 'price-asc':
+            currentProducts = sortByPrice(products);
+            break;
+        case 'price-desc':
+            currentProducts = sortByPriceDesc(products);
+            break;
+        case 'date-asc':
+            currentProducts = sortByDate(products);
+            break;
+        case 'date-desc':
+            currentProducts = sortByDateDesc(products);
+            break;
+        case 'None':
+            break;
+    }
+    render(currentProducts, currentPagination);
 });
